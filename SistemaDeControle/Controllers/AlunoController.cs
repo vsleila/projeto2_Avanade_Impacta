0a42637c8cf074fea39b0f5119b431a46086837e
Leila Mirelle Da Silva Vasconcelos <l.silva.vasconcelos@avanade.com>
2024-04-28 16:39:41 -0300

    Projeto Desenvolvido

---
+++ SistemaDeControle/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaDeControle.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Aluno()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaDeControle/Controllers/CursoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaDeControle.Controllers
+{
+    public class CursoController : Controller
+    {
+        public IActionResult Curso()
+        {
+            return View();
+        }
+    }
+}
